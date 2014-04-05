9964feae2ee88a6bcab5c905de4a77a7e3d48d7c
brettimus <brbeut@gmail.com>
2014-04-05 15:44:07 -0700

    Update server.js
    fixed syntax

--- server.js
+++ server.js
@@ -60,10 +60,10 @@ server.get('/transactions/cash_contributions/:year', function(req, res, next) {
 		res.send(error);
 	})
 	
-server.get('/url_pattern_here', function(req, res, next) {}
+server.get('/url_pattern_here', function(req, res, next) {
 
   // testing query object from the request -BB
   res.send('year: ' + req.query.year);
 
-})
+	})
 })
