c0eeed31a8d453e9c5273b3bbda21e8b94f691e2
Jason Brown <iam@browniefed.com>
2014-04-05 13:33:43 -0700

    Clean up the code some

--- server.js
+++ server.js
@@ -1,37 +1,35 @@
-var restify = require('restify');
-var pg = require('pg');
-var conString = {
-	user: process.env.RDS_USER,
-	database: 'hackoregon',
-	password: process.env.RDS_PASS,
-	post: '5432',
-	host: 'hackoregon.cr7ctfkctdpv.us-west-2.rds.amazonaws.com'
-};
-var server = restify.createServer();
+var restify = require('restify'),
+	pg = require('pg'),
+	connection = {
+		user: process.env.RDS_USER,
+		database: 'hackoregon',
+		password: process.env.RDS_PASS,
+		post: '5432',
+		host: 'hackoregon.cr7ctfkctdpv.us-west-2.rds.amazonaws.com'
+	},
+	server = restify.createServer(),
+	client = new pg.Client(connection);
 
-pg.connect(conString, function(err, client, done) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
-
-	server.get('/test', function(req, res, next) {
-		client.query('select sub_type, sum(amount) from raw_committee_transactions group by sub_type order by sum(amount) desc;', function(err, result) {
-			//call `done()` to release the client back to the pool
-			done();
-			if(err) {
-			res.status(404);
-			return;
-			}
-			res.contentType = 'json';
-			res.send(result);
-			//output: 1
-		});
-	});
-	server.listen(process.env.PORT || 8080, function() {
-		console.log('%s listening at %s', server.name, server.url);
-	});
+	client.connect();
 
 
 
-
+//Setup up the server
+server.listen(process.env.PORT || 8080, function() {
+	//server is listineing
 });
+
+
+server.get('/test', function(req, res, next) {
+	var query = client.query('select sub_type, sum(amount) from raw_committee_transactions group by sub_type order by sum(amount) desc;');
+
+	query.on('row', function(err, data) {
+		if (err) {
+			res.status('404');
+			res.send('DB ERROR');
+			return;
+		}
+		res.contentType = 'json';
+		res.send(data)
+	});
+})
