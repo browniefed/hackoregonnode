b229fbc05424a207c8e6db72b530aeff787f67b5
Jason Brown <iam@browniefed.com>
2014-04-05 12:58:29 -0700

    initial test

--- server.js
+++ server.js
@@ -6,19 +6,37 @@ var conString = {
 	password: process.env.RDS_PASS,
 	post: '5432',
 	host: 'hackoregon.cr7ctfkctdpv.us-west-2.rds.amazonaws.com'
-}
+};
+var server = restify.createServer();
+
 pg.connect(conString, function(err, client, done) {
   if(err) {
     return console.error('error fetching client from pool', err);
   }
-  client.query('select sub_type, sum(amount) from raw_committee_transactions group by sub_type order by sum(amount) desc;', function(err, result) {
-    //call `done()` to release the client back to the pool
-    done();
+  // 
+  // 
+  // 
+  // 
+  // 
+  // 
+  // 
+	server.get('/test', function(req, res, next) {
+		client.query('select sub_type, sum(amount) from raw_committee_transactions group by sub_type order by sum(amount) desc;', function(err, result) {
+			//call `done()` to release the client back to the pool
+			done();
+			if(err) {
+			return console.error('error running query', err);
+			}
+			res.contentType = 'json';
+			res.send(result);
+			//output: 1
+		});
+	});
+	server.listen(process.evn.PORT || 8080, function() {
+		console.log('%s listening at %s', server.name, server.url);
+	});
 
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].numbor);
-    //output: 1
-  });
+
+
+
 });
