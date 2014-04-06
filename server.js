34de34002a91be496237b18e579342ed4758a0ac
Jason Brown <iam@browniefed.com>
2014-04-05 17:13:10 -0700

    It accepts arrays....

--- server.js
+++ server.js
@@ -64,13 +64,13 @@ server.get('/committees', function(req, res, next) {
 	var page = req.query.page || 0,
 		limit = req.query.limit || 100,
 		start = page * limit;
-		query = client.query('SELECT * FROM raw_committees LIMIT $1 OFFSET $2', limit, start);
+		query = client.query('SELECT * FROM raw_committees LIMIT $1 OFFSET $2', [limit,start]);
 	respond(query, res);
 });
 
 server.get('/commitee/:id', function(req, res, next) {
 	var id = req.params.id,
-		query = client.query('SELECT * FROM raw_commitees WHERE comittee_id = $1', id);
+		query = client.query('SELECT * FROM raw_commitees WHERE comittee_id = $1', [id]);
 	respond(query, res);
 });
 server.get('/committees/:id/transactions', function(req, res, next) {
@@ -78,7 +78,7 @@ server.get('/committees/:id/transactions', function(req, res, next) {
 		limit = req.query.limit || 100,
 		start = page * limit,
 		id = req.params.id,
-		query = client.query('SELECT * from raw_committees inner join raw_committee_transactions on committee_id=filer_id WHERE committee_id = $1 LIMIT $2 OFFSET $3', id, limit, start);
+		query = client.query('SELECT * from raw_committees inner join raw_committee_transactions on committee_id=filer_id WHERE committee_id = $1 LIMIT $2 OFFSET $3', [id, limit, start]);
 
 	respond(query, res);
 });
